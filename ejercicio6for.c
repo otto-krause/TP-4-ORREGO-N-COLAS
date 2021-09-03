248ac1bc0229296567a47e4eb721bd16b04435ee
Nicolas, hellodah <87020804+nicolasorregoa@users.noreply.github.com>
2021-09-03 20:07:47 -0300

    Update ejercicio6for.c

--- ejercicio6for.c
+++ ejercicio6for.c
@@ -1,41 +1,22 @@
 /*
-Un curso tiene 30 alumnos identificados con números del 1 al 30 por
+Un curso tiene 30 alumnos identificados con nÃºmeros del 1 al 30 por
 cada alumno se ingresan las notas correspondientes a 10 materias, calcu-
 lar el promedio por cada alumno y el promedio general del curso.
 */
 
 #include<stdio.h>
-#include<stdlib.h>
 int main(){
-	
-	int nota,i,j;
-	float final;
-	
-	for (i=0;i<=30;i++){
-		
-		
-		printf("\ningrese las notas de este alumno: ");
-		int suma=0;
-		for (j=0;j<10;j++){
-			
-			scanf("%d", &nota);
-				
-				suma+=nota;
-				final=suma/10;
-		}			
-		
-			
-			printf("\n\n Su nota final es: %f \n",final);
-			system("pause");
-			system("cls");
-			
-			suma+=nota;
-			final=suma/300;
-			
-			printf("\n\n Su nota final es: %f \n",final);
-			system("pause");
-			system("cls");
-	}
-	
-   return 0;
+  int suma2=0,n,i,j;
+  for(i=0;i<30;i++){
+    printf("ingrese la nota de este alumno: ");
+    int suma=0;
+    for(j=0;j<10;j++){
+      scanf("%d",&n);
+      suma+=n;
+    }
+    printf("el promedio es: %d\n",suma/10);
+    suma2+=suma;
+  }
+  printf("el promedio del curso es: %d",suma2/300);
+  return 0;
 }
